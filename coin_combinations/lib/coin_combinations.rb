64ec07fb113576b23265934c6800d63f5b41842b
Christopher Cahill and Phil Mass <phil@>
2018-11-15 09:38:12 -0800

    counts coins up to 499 cents

--- coin_combinations/lib/coin_combinations.rb
+++ coin_combinations/lib/coin_combinations.rb
@@ -3,13 +3,33 @@ require('pry')
 class Coins
   def combo(amount)
     penny_amount = 1
+    nickel_amount = 5
+    dime_amount = 10
+    quarter_amount = 25
+    dollar_amount = 100
     pennies = 0
-    coin_sum = 0
-    until coin_sum >= amount
-      pennies += 1
-      coin_sum += 1 * penny_amount
+    nickels = 0
+    dimes = 0
+    quarters = 0
+    dollars = 0
+    until amount <= 0
+      if amount >= dollar_amount
+        dollars += 1
+        amount -= dollar_amount
+      elsif amount >= quarter_amount
+        quarters += 1
+        amount -= quarter_amount
+      elsif amount >= dime_amount
+        dimes += 1
+        amount -= dime_amount
+      elsif amount >= nickel_amount
+        nickels += 1
+        amount -= nickel_amount
+      else
+        pennies += 1
+        amount -= penny_amount
+      end
     end
-    # binding.pry
-    [pennies, 0, 0, 0]
+    [pennies, nickels, dimes, quarters, dollars]
   end
 end
--- coin_combinations/spec/coin_combinations_spec.rb
+++ coin_combinations/spec/coin_combinations_spec.rb
@@ -5,6 +5,27 @@ require('pry')
 describe('Coins#combo') do
   it('returns pennies for an amount up to 4 cents') do
     coins = Coins.new()
-    expect(coins.combo(4)).to(eq([4, 0, 0, 0]))
+    expect(coins.combo(4)).to(eq([4, 0, 0, 0, 0]))
   end
+
+  it('returns pennies and nickels for an amount up to 9 cents') do
+    coins = Coins.new()
+    expect(coins.combo(9)).to(eq([4, 1, 0, 0, 0]))
+  end
+
+  it('returns pennies, nickels and dimes for an amount up to 24 cents') do
+    coins = Coins.new()
+    expect(coins.combo(24)).to(eq([4, 0, 2, 0, 0]))
+  end
+
+  it('returns pennies, nickels, dimes, and quarters for an amount up to 99 cents') do
+    coins = Coins.new()
+    expect(coins.combo(99)).to(eq([4, 0, 2, 3, 0]))
+  end
+
+  it('returns pennies, nickels, dimes, quarters, and dollars for an amount up to 499 cents') do
+    coins = Coins.new()
+    expect(coins.combo(499)).to(eq([4, 0, 2, 3, 4]))
+  end
+
 end
