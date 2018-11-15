b7fc57941b7814cb7a2dc40abe4aa1ae357c5d4b
Christopher Cahill and Phil Mass <christopher@>
2018-11-15 10:33:53 -0800

    returns zeros in array up to 5 for composite numbers

--- prime_sifting/lib/prime_sifting.rb
+++ prime_sifting/lib/prime_sifting.rb
@@ -2,6 +2,18 @@ require ('pry')
 
 class Primes
   def sift(user_number)
-    values = *(2..user_number)
+    values = *(0..user_number)
+    values[1] = 0
+    current_prime = 2
+
+    values.map! do |int|
+      if (int != current_prime) && (int % current_prime == 0)
+        0
+      else
+        int
+      end
+    end
+    values
+    # values.select {|v| v > 0}
   end
 end
--- prime_sifting/spec/prime_sifting_spec.rb
+++ prime_sifting/spec/prime_sifting_spec.rb
@@ -5,7 +5,12 @@ require ('pry')
 describe('Primes#sift') do
   it('returns primes up to 3') do
     primes = Primes.new()
-    expect(primes.sift(3)).to(eq([2, 3]))
+    expect(primes.sift(3)).to(eq([0, 0, 2, 3]))
+  end
+
+  it('returns primes up to 5') do
+    primes = Primes.new()
+    expect(primes.sift(5)).to(eq([0, 0, 2, 3, 0, 5]))
   end
 
 end
